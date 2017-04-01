4dd8b999684ff6ca7110c2fd5a5b4534d8afd75d
SimpleviewSoldier <codersgonnacode4life@gmail.com>
2017-04-01 16:41:44 -0700

    Update base.js

--- base.js
+++ base.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 	var view = {
 		name: "Frank",
-		occupation: "Web Developer"
+		occupation: "Web Developer",
+		tits: "http://hiboobs.com/wp-content/uploads/2009/12/tugjobs_bb10.jpg"
 	}
 
-	var output = Mustache.render("{{name}} {{occupation}}", view);
+	var output = Mustache.render("{{tits}}", view);
 	var target = $("#it").html(output);
 });
